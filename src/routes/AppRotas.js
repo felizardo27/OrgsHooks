758e9ecbb769d3beeb0564aa7b47b4abc18afa11
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-05-02 06:05:59 -0300

    feat: Adding icons to tab.navigation

--- src/routes/AppRotas.js
+++ src/routes/AppRotas.js
@@ -3,13 +3,27 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MelhoresProdutoresRotas from './MelhoresProdutoresRotas';
 import ProdutorRotas from './ProdutorRotas';
+import CoracaoIcon from '../assets/coracao.svg';
+import HomeIcon from '../assets/home.svg';
 
 const Tab = createBottomTabNavigator();
 
 export default function AppRotas() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({color}) => {
+            let Icon = HomeIcon;
+            if (route.name === 'Melhores Produtores') {
+              Icon = CoracaoIcon;
+            }
+            return <Icon color={color} />;
+          },
+          headerShown: false,
+          tabBarActiveTintColor: '#2A9F85',
+          tabBarInactiveTintColor: '#C7C7C7',
+        })}>
         <Tab.Screen name="Home" component={ProdutorRotas} />
         <Tab.Screen
           name="Melhores Produtores"
